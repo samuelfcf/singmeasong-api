386c8f86aff9267715db2af57d3041b214fb1307
Bruno <mguimaraesbruno@gmail.com>
2021-12-07 14:36:50 -0300

    Code Review: Brunão

--- src/server.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js';
-import './setup.js';
-
-app.listen(process.env.PORT, () => {
-  console.clear();
-  console.log(`Env: ${process.env.NODE_ENV}`);
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
